4820887ce475109975b65cd15f2f0b060ea88dc7
LOUKHNATI Mohamed Khalil <34715586+medkhabt@users.noreply.github.com>
2021-08-01 07:55:27 +0100

    Prev command fixed when no prev hydration break (#89)
    * Add tests for this functionnality
    
    * Add getDurationSinceLastBreak, formatHandleHydratePrevCommand, change timeSinceLastBreak to Optional, add new field representing the time of loggin.
    
    * Considre resetHydrateTimer as re-logging, so instantiate loginInstance with current time
    
    * Change field name from loginInstance to loginInstant
    
    * Remove unused import
    
    * remove unused imports, and change some documentation
    
    * Fix getting correct message by instantiating lastHydrateInstant when needed.

--- src/test/java/com/hydratereminder/HydrateReminderPluginTest.java
+++ src/test/java/com/hydratereminder/HydrateReminderPluginTest.java
@@ -1,10 +1,12 @@
 package com.hydratereminder;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 import org.junit.Test;
 
 import java.time.Duration;
+import java.util.Optional;
 
 //
 //import mockit.*;
@@ -460,4 +462,31 @@ public class HydrateReminderPluginTest {
         assertEquals("0 seconds",
                 hydrateReminderPlugin.getTimeDisplay(Duration.ofSeconds(0)));
     }
+
+    @Test
+    public void shouldReturnDifferentMessageWhenThereIsNoTimeSinceLastBreak() {
+        final Optional<Duration> timeSinceLastBreak = Optional.empty();
+        final String prevCommandMessage = hydrateReminderPlugin.formatHandleHydratePrevCommand(timeSinceLastBreak);
+        assertEquals("No hydration breaks have been taken yet.", prevCommandMessage);
+    }
+
+    @Test
+    public void shouldReturnNoDurationWhenThereIsNoLastBreak() {
+        final Optional<Duration> timeSinceLastBreak = hydrateReminderPlugin.getDurationSinceLastBreak(Optional.empty());
+        assertFalse(timeSinceLastBreak.isPresent());
+    }
+
+    @Test
+    public void shouldReturnCorrectStringFormatOfHandleHydratePrevCommandMessage() {
+        final Optional<Duration> timeSinceLastBreak = Optional.of(Duration.ofMinutes(130));
+        final String prevCommandMessage = hydrateReminderPlugin.formatHandleHydratePrevCommand(timeSinceLastBreak);
+        assertEquals("2 hours 10 minutes 0 seconds since the last hydration break.", prevCommandMessage);
+    }
+
+    // TODO: Need a static clock in hydrateReminderPlugin so i can mock it in the test to override Instant.now(Clock)
+//    @Test
+//    public void shoudldGetCorrectDurationFromLastBreakTillNowWhenThereIsLastBreak() {
+//        final Instant lastBreakInstant = Instant.now(Clock.fixed())
+////        final Optional<Duration> timeSinceLastBreak = hydrateReminderPlugin.getDurationSinceLastBreak();
+//    }
 }
