73c8085c3bc2db7a0dded70f005efec8d3481bdd
Marcin Słowiak <marcin.slowiak.007@gmail.com>
2022-07-30 10:02:03 +0200

    Move messages to dictionary class

---
+++ src/main/java/com/hydratereminder/HydrateBreakMessageDictionary.java
@@ -0,0 +1,53 @@
+package com.hydratereminder;
+
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+class HydrateBreakMessageDictionary {
+    /**
+     * Hydrate Reminder interval break text to display
+     */
+    private static final List<String> HYDRATE_BREAK_TEXT_LIST =
+            Collections.unmodifiableList(
+                    new ArrayList<String>() {{
+                        add("It's time for a quick hydration break");
+                        add("Dehydration causes fatigue so take a hydration break");
+                        add("It's time to drink some good ol' water");
+                        add("Stay healthy by taking a hydration break");
+                        add("Time to glug, glug, glug some water");
+                        add("It is now time to take a hydration break");
+                        add("Time to hydrate");
+                        add("Power up with a hydration break now");
+                        add("Got water? It's time to hydrate");
+                        add("Cheers to this hydration break");
+                        add("Hydration time is now");
+                        add("Fuel up with a hydration break");
+                        add("3... 2... 1... It's hydration time");
+                        add("Feeling parched yet? It's hydration time");
+                        add("Now would be a fantastic time to hydrate");
+                        add("Water... you need... water");
+                        add("Thirsty? Time to grab a drink");
+                        add("Water: a liquid that is necessary for life. Why don't you drink some");
+                        add("Hello, this is your reminder to take a hydration break");
+                        add("Remember to stay hydrated");
+                        add("What'cha drinking there? It's time to take another sip");
+                        add("Ding ding ding! Hydration time");
+                        add("Everyone needs water, you should drink some now");
+                        add("Time for another glass of water");
+                        add("You've been grinding hard, time to reward your self with a hydration break");
+                        add("Water makes the world go 'round, you should drink some now");
+                        add("Hydration can improve your ability to focus, time for a hydration break");
+                        add("Time to take a break and hydrate");
+                        add("Dehydration can cause you to feel dizzy and lightheaded, take a hydration break");
+                        add("Dehydration can cause dry mouth, lips, and eyes. Take a hydration break");
+                    }});
+
+    public static String getRandomHydrateBreakMessage() {
+        final SecureRandom randomGenerator = new SecureRandom();
+        final int randomNumber = randomGenerator.nextInt(HYDRATE_BREAK_TEXT_LIST.size());
+        return HYDRATE_BREAK_TEXT_LIST.get(randomNumber);
+    }
+
+}
--- src/main/java/com/hydratereminder/HydrateReminderPlugin.java
+++ src/main/java/com/hydratereminder/HydrateReminderPlugin.java
@@ -67,44 +67,6 @@ import java.util.List;
 public class HydrateReminderPlugin extends Plugin
 {
 	/**
-	 * Hydrate Reminder interval break text to display
-	 */
-	private static final List<String> HYDRATE_BREAK_TEXT_LIST =
-			Collections.unmodifiableList(
-					new ArrayList<String>() {{
-						add("It's time for a quick hydration break");
-						add("Dehydration causes fatigue so take a hydration break");
-						add("It's time to drink some good ol' water");
-						add("Stay healthy by taking a hydration break");
-						add("Time to glug, glug, glug some water");
-						add("It is now time to take a hydration break");
-						add("Time to hydrate");
-						add("Power up with a hydration break now");
-						add("Got water? It's time to hydrate");
-						add("Cheers to this hydration break");
-						add("Hydration time is now");
-						add("Fuel up with a hydration break");
-						add("3... 2... 1... It's hydration time");
-						add("Feeling parched yet? It's hydration time");
-						add("Now would be a fantastic time to hydrate");
-						add("Water... you need... water");
-						add("Thirsty? Time to grab a drink");
-						add("Water: a liquid that is necessary for life. Why don't you drink some");
-						add("Hello, this is your reminder to take a hydration break");
-						add("Remember to stay hydrated");
-						add("What'cha drinking there? It's time to take another sip");
-						add("Ding ding ding! Hydration time");
-						add("Everyone needs water, you should drink some now");
-						add("Time for another glass of water");
-						add("You've been grinding hard, time to reward your self with a hydration break");
-						add("Water makes the world go 'round, you should drink some now");
-						add("Hydration can improve your ability to focus, time for a hydration break");
-						add("Time to take a break and hydrate");
-						add("Dehydration can cause you to feel dizzy and lightheaded, take a hydration break");
-						add("Dehydration can cause dry mouth, lips, and eyes. Take a hydration break");
-					}});
-
-	/**
 	 * Hydrate Reminder startup welcome text to display
 	 */
 	private static final List<String> HYDRATE_WELCOME_TEXT_LIST =
@@ -655,10 +617,8 @@ public class HydrateReminderPlugin extends Plugin
 	 */
 	private String getHydrateReminderMessage()
 	{
-		final Random randomGenerator = new Random();
 		final String playerName = Objects.requireNonNull(client.getLocalPlayer()).getName();
-		final String hydrateReminderMessage = HYDRATE_BREAK_TEXT_LIST.get(
-				randomGenerator.nextInt(HYDRATE_BREAK_TEXT_LIST.size()));
+		final String hydrateReminderMessage = HydrateBreakMessageDictionary.getRandomHydrateBreakMessage();
 		return String.format("%s, %s.", hydrateReminderMessage, playerName);
 	}
 
