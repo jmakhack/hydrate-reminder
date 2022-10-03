b22a0ef43b5c40c49efe8ca17b05caa61865a262
Marcin Słowiak <marcin.slowiak.007@gmail.com>
2022-10-03 08:39:13 +0200

    Issue 214: Extract next command logic to handler (#239)
    * Issue 214: Extract next command logic to handler
    
    * Issue 214: Extract next command logic to handler

--- src/main/java/com/hydratereminder/HydrateReminderPlugin.java
+++ src/main/java/com/hydratereminder/HydrateReminderPlugin.java
@@ -265,7 +265,7 @@ public class HydrateReminderPlugin extends Plugin
 					switch (arg)
 					{
 						case NEXT:
-							handleHydrateNextCommand();
+							commandDelegate.invokeCommand(commandExecuted);
 							break;
 						case PREV:
 							handleHydratePrevCommand();
@@ -295,21 +295,6 @@ public class HydrateReminderPlugin extends Plugin
 		}
 		// TODO: Uncomment when the commands will be refactored to com.hydratereminder.command
 		// commandDelegate.invokeCommand(commandExecuted);
-	}
-
-	/**
-	 * <p>Handle the hydrate next command by generating a chat message displaying the amount of time
-	 * until the next hydration break
-	 * </p>
-	 * @since 1.1.0
-	 */
-	private void handleHydrateNextCommand()
-	{
-		final Instant nextHydrateReminderInstant = getNextHydrateReminderInstant();
-		final Duration timeUntilNextBreak = Duration.between(Instant.now(), nextHydrateReminderInstant);
-		final String timeString = getTimeDisplay(timeUntilNextBreak);
-		final String message = timeString + " until the next hydration break.";
-		chatMessageSender.sendHydrateEmojiChatGameMessage(message);
 	}
 
 	/**
@@ -366,7 +351,7 @@ public class HydrateReminderPlugin extends Plugin
 	 * @return the time in string format
 	 * @since 1.1.1
 	 */
-	protected String getTimeDisplay(Duration duration)
+	public String getTimeDisplay(Duration duration)
 	{
 		final int hours = Math.toIntExact(duration.toHours());
 		final int minutes = Math.toIntExact(duration.toMinutes() % 60);
@@ -486,7 +471,7 @@ public class HydrateReminderPlugin extends Plugin
 	 * @return the instant to send the next hydrate reminder on
 	 * @since 1.1.0
 	 */
-	protected Instant getNextHydrateReminderInstant()
+	public Instant getNextHydrateReminderInstant()
 	{
 		final Duration hydrateReminderDuration = Duration.ofMinutes(config.hydrateReminderInterval());
 		if(getLastHydrateInstant().isPresent())
