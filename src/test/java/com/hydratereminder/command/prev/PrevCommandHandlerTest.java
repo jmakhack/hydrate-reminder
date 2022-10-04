742bcfbe284fc5f096f99734091805fb7fff875b
Maciej Słowiak <94566117+macslowiak@users.noreply.github.com>
2022-10-04 12:46:57 +0200

    Extract prev command logic to proper handler  (#256)
    * Extract prev command logic
    
    * Add Clock to make classes easier to test
    
    Adding clock helps to test methods with .now() ex. Instant.now(clock) or DateTime.now(clock)
    
    * Add and modify unit tests

---
+++ src/test/java/com/hydratereminder/command/prev/PrevCommandHandlerTest.java
@@ -0,0 +1,110 @@
+package com.hydratereminder.command.prev;
+
+import com.hydratereminder.HydrateReminderPlugin;
+import com.hydratereminder.chat.ChatMessageSender;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.time.Clock;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
+public class PrevCommandHandlerTest {
+
+    @Mock
+    private ChatMessageSender chatMessageSender;
+
+    @Mock
+    private HydrateReminderPlugin hydrateReminderPlugin;
+
+    @Mock
+    private Clock clock;
+
+    @InjectMocks
+    private PrevCommandHandler prevCommandHandler;
+
+
+    @Test
+    public void shouldSendMessageSinceLastHydrationInterval() {
+        //given
+        final Optional<Duration> timeSinceLastBreak = Optional.of(Duration.ofMinutes(100));
+        final String timeSinceLastBreakAsString = "1 hours 40 minutes 0 seconds";
+        final Optional<Instant> lastHydrateTime = Optional.of(Instant.now().minus(10, ChronoUnit.MINUTES));
+
+        given(clock.instant()).willReturn(Instant.MAX);
+        given(hydrateReminderPlugin.getLastHydrateInstant()).willReturn(lastHydrateTime);
+        given(hydrateReminderPlugin.getDurationSinceLastBreak(lastHydrateTime, Instant.now(clock))).willReturn(timeSinceLastBreak);
+        given(hydrateReminderPlugin.getTimeDisplay(timeSinceLastBreak.get())).willReturn(timeSinceLastBreakAsString);
+        given(hydrateReminderPlugin.isResetState()).willReturn(true);
+
+        String expectedMessage = "1 hours 40 minutes 0 seconds since the last hydration interval reset.";
+
+        //when
+        prevCommandHandler.handle();
+
+        //then
+        verify(chatMessageSender).sendHydrateEmojiChatGameMessage(expectedMessage);
+    }
+
+    @Test
+    public void shouldReturnExpectedMessageWhenThereIsNoTimeSinceLastBreak() {
+        //given
+        final Optional<Duration> timeSinceLastBreak = Optional.empty();
+
+        String expectedMessage = "No hydration breaks have been taken yet.";
+
+        //when
+        final String prevCommandMessage = prevCommandHandler.formatHandleHydratePrevCommand(timeSinceLastBreak);
+
+        //then
+        assertEquals(expectedMessage, prevCommandMessage);
+    }
+
+    @Test
+    public void shouldReturnExpectedMessageWhenThereIsResetSinceLastBreak() {
+        //given
+        final Optional<Duration> timeSinceLastBreak = Optional.of(Duration.ofSeconds(645));
+        final String timeSinceLastBreakAsString = "10 minutes 45 seconds";
+
+        given(hydrateReminderPlugin.getTimeDisplay(timeSinceLastBreak.get())).willReturn(timeSinceLastBreakAsString);
+        given(hydrateReminderPlugin.isResetState()).willReturn(true);
+
+        String expectedMessage = "10 minutes 45 seconds since the last hydration interval reset.";
+
+        //when
+        final String prevCommandMessage = prevCommandHandler.formatHandleHydratePrevCommand(timeSinceLastBreak);
+
+        //then
+        assertEquals(expectedMessage, prevCommandMessage);
+    }
+
+    @Test
+    public void shouldReturnCorrectStringFormatOfHandleHydratePrevCommandMessage() {
+        //given
+        final Optional<Duration> timeSinceLastBreak = Optional.of(Duration.ofMinutes(130));
+        final String timeSinceLastBreakAsString = "2 hours 10 minutes 0 seconds";
+
+        given(hydrateReminderPlugin.getTimeDisplay(timeSinceLastBreak.get())).willReturn(timeSinceLastBreakAsString);
+        given(hydrateReminderPlugin.isResetState()).willReturn(false);
+
+        String expectedStringFormat = "2 hours 10 minutes 0 seconds since the last hydration break.";
+
+        //when
+        final String prevCommandMessage = prevCommandHandler.formatHandleHydratePrevCommand(timeSinceLastBreak);
+
+        //then
+        assertEquals(expectedStringFormat, prevCommandMessage);
+    }
+
+}
+
