6ac52a290905af93f4b930215e9b6fa23ae20052
Jacoberson <c.jacob.moore92@gmail.com>
2022-10-05 18:01:24 -0500

    finalize tests

--- src/test/java/com/hydratereminder/dictionary/HydrateBreakMessageDictionaryTest.java
+++ src/test/java/com/hydratereminder/dictionary/HydrateBreakMessageDictionaryTest.java
@@ -1,12 +1,9 @@
 package com.hydratereminder.dictionary;
 
 import com.hydratereminder.HydrateReminderPersonalityType;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.EnumSource;
-import org.mockito.Mock;
 import org.mockito.MockedConstruction;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -15,7 +12,6 @@ import java.security.SecureRandom;
 
 import static com.hydratereminder.dictionary.HydrateBreakMessageDictionary.getRandomHydrateBreakMessageForPersonality;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 @ExtendWith(MockitoExtension.class)
 public class HydrateBreakMessageDictionaryTest {
@@ -30,16 +26,19 @@ public class HydrateBreakMessageDictionaryTest {
             "POLITE | Your beautiful skin will shine brighter when hydrated. Please go ahead | 1",
             "PIRATE | Arrr, the ocean reminds me, that you should take a water break! | 6",
             "MOTIVATIONAL | Thousands Have Lived Without Love, Not One Without Water. Take a sip | 13",
-            "AGGRESSIVE | You gotta drink some Water, Yeah! | 9"})
-    public void shouldChooseCorrectMessageBasedOnPersonalityType(
-            HydrateReminderPersonalityType personalityType, String expectedMessage, int value) {
+            "AGGRESSIVE | You gotta drink some Water, Yeah! | 9",
+    })
+    public void shouldChooseCorrectMessageBasedOnPersonalityType (
+            HydrateReminderPersonalityType personalityType, String expectedMessage, int value)
+    {
+            try (MockedConstruction<SecureRandom> ignored = Mockito.mockConstruction(SecureRandom.class, (mock, context) ->
+                    Mockito.when(mock.nextInt(Mockito.anyInt())).thenReturn(value)
+            ))
+            {
+                String givenMessage = getRandomHydrateBreakMessageForPersonality(personalityType);
 
-        try (MockedConstruction<SecureRandom> mockRandom = Mockito.mockConstruction(SecureRandom.class, (mock, context) ->
-                Mockito.when(mock.nextInt(Mockito.anyInt())).thenReturn(value)
-        )) {
-            String givenMessage = getRandomHydrateBreakMessageForPersonality(personalityType);
-
-            assertEquals(givenMessage, expectedMessage);
-        }
+                assertEquals(givenMessage, expectedMessage);
+            }
     }
+
 }
