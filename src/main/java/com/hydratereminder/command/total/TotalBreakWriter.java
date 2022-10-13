6bf4dce10b943675b1e29397c4b138a93aebbd1a
Jacoberson <c.jacob.moore92@gmail.com>
2022-10-13 18:47:47 -0500

    remove unused import

--- src/main/java/com/hydratereminder/command/total/TotalBreakWriter.java
+++ src/main/java/com/hydratereminder/command/total/TotalBreakWriter.java
@@ -9,7 +9,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
