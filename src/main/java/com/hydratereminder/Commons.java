c13438ffba6ffeb5bbe31a350a1007fa3702bdee
Jacoberson <58759808+Jacoberson@users.noreply.github.com>
2022-10-15 02:43:45 -0500

    Add write to json (#350)
    * update description for animation setting
    
    * update runelite version
    
    * change file type to json
    
    * change write method to write json
    
    * change read method to read json
    
    * fix codacy issues
    
    * remove unused import
    
    * move files to commons.java and rename file

--- src/main/java/com/hydratereminder/Commons.java
+++ src/main/java/com/hydratereminder/Commons.java
@@ -1,11 +1,26 @@
 package com.hydratereminder;
 
+import java.io.File;
+
+import static net.runelite.client.RuneLite.RUNELITE_DIR;
+
 public class Commons {
     /**
      * Main command name for the Hydrate Reminder plugin
      */
     public static final String HYDRATE_COMMAND_NAME = "hydrate";
     public static final String HYDRATE_COMMAND_ALIAS = "hr";
+
+    /**
+     * <p>Directory for Hydrate Reminder</p>
+     */
+    public static final File HYDRATION_REMINDER_DIR = new File(RUNELITE_DIR, "hydrateReminder");
+
+    /**
+     * <p>File for total hydration breaks</p>
+     */
+    public static final File HYDRATION_REMINDER_BREAKS_FILE =
+            new File(HYDRATION_REMINDER_DIR, "hydrateReminderStats.json");
 
     /**
      * Prefix for all chat commands in RuneLite
