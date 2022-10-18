0bcf414d072240c5e2dff602eeff53250d7ceb96
Jacoberson <c.jacob.moore92@gmail.com>
2022-10-18 14:09:18 -0500

    parameterize commandArgs tests

--- src/test/java/com/hydratereminder/HydrateReminderCommandArgsTest.java
+++ src/test/java/com/hydratereminder/HydrateReminderCommandArgsTest.java
@@ -2,53 +2,56 @@ package com.hydratereminder;
 
 import com.hydratereminder.command.NotRecognizedCommandException;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class HydrateReminderCommandArgsTest {
-    @Test
-    void testCommandArgsToString() {
-        assertEquals("next", HydrateReminderCommandArgs.NEXT.toString());
-        assertEquals("prev", HydrateReminderCommandArgs.PREV.toString());
-        assertEquals("reset", HydrateReminderCommandArgs.RESET.toString());
-        assertEquals("hydrate", HydrateReminderCommandArgs.HYDRATE.toString());
-        assertEquals("help", HydrateReminderCommandArgs.HELP.toString());
-        assertEquals("total", HydrateReminderCommandArgs.TOTAL.toString());
+    @ParameterizedTest
+    @CsvSource({
+            "next, NEXT",
+            "prev, PREV",
+            "reset, RESET",
+            "hydrate, HYDRATE",
+            "help, HELP",
+            "total, TOTAL"
+    })
+    void testCommandArgsToString(String expectedString, HydrateReminderCommandArgs commandArg) {
+        assertEquals(expectedString, commandArg.toString());
     }
 
-    @Test
-    void testAliases() {
-        assertEquals("next", HydrateReminderCommandArgs.NEXT.getCommandArg());
-        assertEquals("prev", HydrateReminderCommandArgs.PREV.getCommandArg());
-        assertEquals("reset", HydrateReminderCommandArgs.RESET.getCommandArg());
-        assertEquals("hydrate", HydrateReminderCommandArgs.HYDRATE.getCommandArg());
-        assertEquals("help", HydrateReminderCommandArgs.HELP.getCommandArg());
-        assertEquals("total", HydrateReminderCommandArgs.TOTAL.getCommandArg());
-
-        assertEquals("n", HydrateReminderCommandArgs.NEXT.getCommandArgAbbr());
-        assertEquals("p", HydrateReminderCommandArgs.PREV.getCommandArgAbbr());
-        assertEquals("r", HydrateReminderCommandArgs.RESET.getCommandArgAbbr());
-        assertEquals("hr", HydrateReminderCommandArgs.HYDRATE.getCommandArgAbbr());
-        assertEquals("h", HydrateReminderCommandArgs.HELP.getCommandArgAbbr());
-        assertEquals("t", HydrateReminderCommandArgs.TOTAL.getCommandArgAbbr());
+    @ParameterizedTest
+    @CsvSource({
+            "n, NEXT",
+            "p, PREV",
+            "r, RESET",
+            "hr, HYDRATE",
+            "h, HELP",
+            "t, TOTAL"
+    })
+    void testCommandArgAbbrToString(String expectedAbbr, HydrateReminderCommandArgs commandArg) {
+        assertEquals(expectedAbbr, commandArg.getCommandArgAbbr());
     }
 
-    @Test
-    void testGetValue() {
-        assertEquals(HydrateReminderCommandArgs.NEXT, HydrateReminderCommandArgs.getValue("next"));
-        assertEquals(HydrateReminderCommandArgs.PREV, HydrateReminderCommandArgs.getValue("prev"));
-        assertEquals(HydrateReminderCommandArgs.RESET, HydrateReminderCommandArgs.getValue("reset"));
-        assertEquals(HydrateReminderCommandArgs.HYDRATE, HydrateReminderCommandArgs.getValue("hydrate"));
-        assertEquals(HydrateReminderCommandArgs.HELP, HydrateReminderCommandArgs.getValue("help"));
-        assertEquals(HydrateReminderCommandArgs.TOTAL, HydrateReminderCommandArgs.getValue("total"));
-
-        assertEquals(HydrateReminderCommandArgs.NEXT, HydrateReminderCommandArgs.getValue("n"));
-        assertEquals(HydrateReminderCommandArgs.PREV, HydrateReminderCommandArgs.getValue("p"));
-        assertEquals(HydrateReminderCommandArgs.RESET, HydrateReminderCommandArgs.getValue("r"));
-        assertEquals(HydrateReminderCommandArgs.HYDRATE, HydrateReminderCommandArgs.getValue("hr"));
-        assertEquals(HydrateReminderCommandArgs.HELP, HydrateReminderCommandArgs.getValue("h"));
-        assertEquals(HydrateReminderCommandArgs.TOTAL, HydrateReminderCommandArgs.getValue("t"));
+    @ParameterizedTest
+    @CsvSource({
+            "next, NEXT",
+            "prev, PREV",
+            "reset, RESET",
+            "hydrate, HYDRATE",
+            "help, HELP",
+            "total, TOTAL",
+            "n, NEXT",
+            "p, PREV",
+            "r, RESET",
+            "hr, HYDRATE",
+            "h, HELP",
+            "t, TOTAL"
+    })
+    void testGetValue(String expectedValue, HydrateReminderCommandArgs commandArg) {
+        assertEquals(HydrateReminderCommandArgs.getValue(expectedValue), commandArg);
     }
 
     @Test
